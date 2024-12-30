f229705e06a01bd29de259fe89724bb21d2ba580
Andrea <andream@fasys.it>
2024-12-30 18:57:29 +0100

    Create favorite-query-filesystem.php

---
+++ plugins/favorite-query-filesystem.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Favorite Query using persistent file system
+ *
+ * This plugin works with the favorite-query plugin: instead of saving queries in local storage, it saves queries in a persistent file system.
+ * Remove this file if you want to save favorite queries in local storage.
+ *
+ *
+ * @author Andrea Mariani, fasys.it
+ */
+class AdminerFavoriteQueryFileSystem
+{
+
+}
